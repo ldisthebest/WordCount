affc0d16eb3819f9665c5f90fb80c44cc77908f5
ldisthebest <981824007@qq.com>
2016-10-20 17:02:36 +0800

    修改了基本功能的输入方式

--- WordCount/src/WordCount.java
+++ WordCount/src/WordCount.java
@@ -12,34 +12,27 @@ import java.util.ArrayList;
 
 
 public class WordCount {
-	private int OutInstruNum;
-	private String[] keyWordOut;
 	private ArrayList keyWord;
+	private ArrayList instru;
+	private String stopFile,outFile,readFile;
 	private String fileWord;
-	public WordCount(int instruNum){
-		OutInstruNum = instruNum;
+	public WordCount(){
 		fileWord = "";
+		stopFile = "";
+		outFile = "";
+		readFile = "";
 		keyWord = new ArrayList<String>();
-		keyWordOut = new String[OutInstruNum];
-		for(int p = 0;p<OutInstruNum;p++){    //初始化数组
-			keyWordOut[p] = "";
-		}
-		
+		instru = new ArrayList<String>();
+
 	}
 	
 	public static void main(String[] args) throws IOException{
-		WordCount wc = new WordCount(3);
-		if(!wc.InputBaseInstruction()||!wc.InputOutInstruction()){
+		WordCount wc = new WordCount();
+		if(!wc.InputBaseInstruction()){
 			return;
 		}		
 		wc.GetFile();
 		wc.OutputResultToFile();
-		
-		//System.out.println(wc.fileWord);
-		//System.out.println(wc.FindRowlineNum());
-		
-//		wc.InputOutInstruction();
-//		wc.OutputNumToFile(2,"-w");
 		
 	}
 	Boolean InputBaseInstruction() throws IOException{
@@ -56,56 +49,58 @@ public class WordCount {
 			}
 			keyWord.add(keyWords);
 		}
-		if((keyWord.size() > 5 ||keyWord.size() < 3)||!((String)keyWord.get(0)).equals("wc.exe")){
+		if(!((String)keyWord.get(0)).equals("wc.exe")){
 			InstruError();
 			return false;
 		}
-		for(int num = 1;num < keyWord.size()-1;num++){
-			if(!IsSecondInstru((String)keyWord.get(num))){
+		for(int i = 1;i<keyWord.size();i++){
+			if(IsSpecialInstru((String)keyWord.get(i))&&i!=1){
 				InstruError();
 				return false;
+			}			
+			if(IsNormalInstru((String)keyWord.get(i))||IsSpecialInstru((String)keyWord.get(i))){
+				instru.add((String)keyWord.get(i));
+			}
+			else if(IsInstruOfStop((String)keyWord.get(i))){
+				instru.add((String)keyWord.get(i));
+				if(++i>keyWord.size()-1){
+					InstruError();
+					return false;
+				}
+				stopFile = (String)keyWord.get(i);
+			}
+			else if(IsInstruOfOut((String)keyWord.get(i))){
+				instru.add((String)keyWord.get(i));
+				if(++i>keyWord.size()-1){
+					InstruError();
+					return false;
+				}
+				outFile = (String)keyWord.get(i);
+			}
+			else{
+				readFile = (String)keyWord.get(i);
 			}
 		}
 
 		return true;
 	}
-	Boolean InputOutInstruction() throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String str = br.readLine();
-		char strC[] = str.toCharArray();
-		int j = 0;
-		for(int i = 0;i<OutInstruNum;i++){
-			while(j<strC.length&&strC[j] != ' '){
-				keyWordOut[i] += ""+strC[j];
-				j++;
-			}
-			//System.out.print(keyWord[i]+" ");
-			j++;
-		}
-		if(--j < strC.length){
-			while(j<strC.length){
-				if(strC[j] != ' '){
-					InstruError();
-					return false;
-				}
-				else{
-					j++;
-				}
-			}
-		}
-		if(!keyWordOut[0].equals("wc.exe")||!IsSencondInstruOfOut(keyWordOut[1])){
-			InstruError();
-			return false;
-		}
-		return true;
-	}
-	Boolean IsSecondInstru(String s){
-		if(s.equals("-c")||s.equals("-w")||s.equals("-l"))
+	Boolean IsNormalInstru(String s){
+		if(s.equals("-c")||s.equals("-w")||s.equals("-l")||s.equals("-a"))
 			return true;
 		return false;
 	}
-	Boolean IsSencondInstruOfOut(String s){
+	Boolean IsSpecialInstru(String s){
+		if(s.equals("-s"))
+			return true;
+		return false;
+	}
+	Boolean IsInstruOfOut(String s){
 		if(s.equals("-o"))
+			return true;
+		return false;
+	}
+	Boolean IsInstruOfStop(String s){
+		if(s.equals("-e"))
 			return true;
 		return false;
 	}
@@ -113,7 +108,7 @@ public class WordCount {
 		System.out.println("指令格式不正确");
 	}
 	void GetFile() throws IOException{
-		InputStream file = new FileInputStream((String)keyWord.get(keyWord.size()-1));
+		InputStream file = new FileInputStream(readFile);
 		int ch = file.read();
 		while(ch != -1){
 			fileWord += ""+(char)ch;
@@ -132,7 +127,7 @@ public class WordCount {
 		return num;
 	}
 	Boolean IsWordC(char c){
-		if(c != ' ' && c != '\n' && c != ',' &&c != '\t'){
+		if(c != ' ' && c != '\n' && c != ',' &&c != '\t'&&c != '\r'){
 			return true;
 		}
 		return false;
@@ -142,11 +137,12 @@ public class WordCount {
 		char character[] = fileWord.toCharArray();
 		for(int i = 0;i<character.length;i++){
 			if(IsWordC(character[i])){
-				num++;	  
+				num++;	
+				while(i<character.length && IsWordC(character[i])){
+					i++;
+				}
 			}
-			while(i<character.length && IsWordC(character[i])){
-				i++;
-			}
+			
 		}
 		return num;
 		
@@ -162,17 +158,18 @@ public class WordCount {
 	}
 	void OutputResultToFile() throws IOException{
 		String result = "";
-		for(int num = 1;num < keyWord.size()-1;num++){
-			switch((String)keyWord.get(num)){
-			case "-c":result += "字符数量为："+FindCharacterNum()+"\n";break;
-			case "-w":result += "单词数量为："+FindWordNum()+"\n";break;
-			case "-l":result += "总行数为："+FindRowlineNum()+"\n";break;
+		for(int num = 0;num < instru.size()-1;num++){
+			switch((String)instru.get(num)){
+			case "-c":result += readFile+",字符数量为："+FindCharacterNum()+"\n";break;
+			case "-w":result += readFile+",单词数量为："+FindWordNum()+"\n";break;
+			case "-l":result += readFile+",总行数为："+FindRowlineNum()+"\n";break;
 			}
 		}
-		File f = new File(keyWordOut[2]);
+		File f = new File(outFile);
 		FileWriter file = new FileWriter(f);
 		file.write(result);
 		file.close();
+		System.out.print("输出完毕，可查看指定文件");
 	}
 
 }
--- ad.c
+++ ad.c
@@ -1,11 +1,2 @@
-as^213 asd 123&%
-sa
-asd
-asda
 as
-d
-ad
-a
-fasfasf
-adassa
-   ADAS
+sa
